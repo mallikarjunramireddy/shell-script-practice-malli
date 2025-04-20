9c748dfc37070f9a061150aa47fba133a5f2c8e1
mallikarjunramireddy <mallikarjunrao.ramireddy@gmail.com>
2025-04-20 19:09:16 +0530

    malli-shell-practice

---
+++ 09-install-mysql.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+then
+    echo "Please run this script as root user"
+    exit 1
+else
+    echo "You are running this script as root user"
+fi
+
+yum install mysql -y
+
+if [ $? -eq 0 ]
+then
+    echo "MySQL Installation..... successfully"
+else
+    echo "MySQL Installation...... failed"
+fi
+
+yum install git -y
+
+if [ $? -eq 0 ]
+then
+    echo "Git installation.... successfully"
+else
+    echo "Git installation.... failed"
+fi
+
+
+
