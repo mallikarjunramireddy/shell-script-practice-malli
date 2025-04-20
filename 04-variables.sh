80f5f39e70545e254dced7b3554aa92ec439cbc9
mallikarjunramireddy <mallikarjunrao.ramireddy@gmail.com>
2025-04-20 18:02:07 +0530

    malli-shell-practice

--- 04-variables.sh
+++ 04-variables.sh
@@ -4,7 +4,7 @@ PERSON1=$1
 PERSON2=$2
 # define variables and use that variables in script
 
-echo "$$1: Hello $$2, Good Morning"
-echo "$$2: Hi $$1, very Good Morning"
-echo "$$1: How are you $$2?"
-echo "$$2: I am good $$1, How are you"
+echo "$1: Hello $2, Good Morning"
+echo "$2: Hi $1, very Good Morning"
+echo "$1: How are you $2?"
+echo "$2: I am good $1, How are you"
