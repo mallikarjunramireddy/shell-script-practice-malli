0490658e397ff96780436af76ce6eea45b2829bb
mallikarjunramireddy <mallikarjunrao.ramireddy@gmail.com>
2025-04-20 17:52:14 +0530

    malli-shell-practice

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,3 +3,6 @@
 DATE=$(date)
 echo "Current date and time is: $DATE"
 echo "Current date and time is: ${DATE}"
+DISK_SPCE=$(df -hT)
+echo "Disk space usage is: $DISK_SPCE"
+echo "Disk space usage is: ${DISK_SPCE}"
