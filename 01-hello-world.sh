532c34c9b889a55fda5a2aec15349fb603f2634d
mallikarjunramireddy <mallikarjunrao.ramireddy@gmail.com>
2025-04-20 14:09:01 +0530

    malli-shell-practice

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 # This is a simple shell script that prints "Hello, World!" to the console.
 echo "Hello Malli, you are currently working as devops engineer"
+set -x
+set -e
+set -o pipefail
+
+df -hT
+ps -ef
+top
+find /home/ec2-user -name 76s-shell-script
+cat /home/ec2-user/sample.txt|grep -i "malli"
+cat /home/ec2-user/sample.txt|awk '{print $1F}'
+
