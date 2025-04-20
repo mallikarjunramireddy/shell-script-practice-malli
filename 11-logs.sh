a08560a08521157dcc275cd9b1566882264afa21
mallikarjunramireddy <mallikarjunrao.ramireddy@gmail.com>
2025-04-20 19:43:37 +0530

    malli-shell-practice

---
+++ 11-logs.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE() {
+if [ $1 -eq 0 ]
+then
+    echo " $2.....$G successfully $N"
+else
+    echo "ERROR:$2......$R failed $N"
+    exit 1
+fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:$R Please run this script as root user $N"
+    exit 1
+else
+    echo "You are running this script as root user"
+fi
+
+yum install mariadb -y &>> $LOGFILE
+
+VALIDATE $? "MARIADB Installation"
+
+
+yum install git -y &>> $LOGFILE
+
+VALIDATE $? "GIT Installation"
+
