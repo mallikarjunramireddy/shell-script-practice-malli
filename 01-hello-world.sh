0cbb333d9a746336d086b0dcb99b60b91d73f31f
mallikarjunramireddy <mallikarjunrao.ramireddy@gmail.com>
2025-04-20 14:12:24 +0530

    malli-shell-practice

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -6,9 +6,12 @@ set -e
 set -o pipefail
 
 df -hT
+
 ps -ef
-top
+
 find /home/ec2-user -name 76s-shell-script
+
 cat /home/ec2-user/sample.txt|grep -i "malli"
+
 cat /home/ec2-user/sample.txt|awk '{print $1F}'
 
