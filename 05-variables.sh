d55b759a3923becfeb4626414cba48d5425f59d4
mallikarjunramireddy <mallikarjunrao.ramireddy@gmail.com>
2025-04-20 18:11:58 +0530

    malli-shell-practice

--- 05-variables.sh
+++ 05-variables.sh
@@ -7,7 +7,14 @@
 #Usage: sh 05-variables.sh
 
 echo "Enter your name:"
+# -s is for silent mode, which means the input will not be displayed on the screen
 read -s USERNAME
 echo "Enter password:"
 read -s PASSWORD
+echo "Hello $USERNAME, your password is $PASSWORD"
+
+echo "Enter your name:"
+read  USERNAME
+echo "Enter password:"
+read  PASSWORD
 echo "Hello $USERNAME, your password is $PASSWORD"
