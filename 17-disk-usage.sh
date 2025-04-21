718a4c2315fc8c2de8d2d334d0878c593b1c559e
mallikarjunramireddy <mallikarjunrao.ramireddy@gmail.com>
2025-04-21 14:07:21 +0530

    malli-shell-practice

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -7,7 +7,7 @@ message=""
 
 while IFS= read -r line
 do 
-    usage=$(echo $line|awk '{print $6F}'|cut -d % f1)
+    usage=$(echo $line|awk '{print $6F}'|cut -d % -f1)
     partition=$(echo $line|awk '{print $1F}')
 if [ $usage -ge $TRESHHOLD ]
  then
