26d346055a31c3d8c324791293c6b1abf190b8d9
mallikarjunramireddy <mallikarjunrao.ramireddy@gmail.com>
2025-04-21 14:14:43 +0530

    malli-shell-practice

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -7,8 +7,10 @@ message=""
 
 while IFS= read -r line
 do 
-    usage=$(echo $line|awk '{print $6F}'|cut -d % -f1)
-    partition=$(echo $line|awk '{print $1F}')
+    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+
+    partition=$(echo $line | awk '{print $1F}')
+    
 if [ $usage -ge $TRESHHOLD ]
  then
      message+="High Disk Usage on $partition: $usage"
@@ -16,5 +18,5 @@ fi
 
 done <<< $DISK_USAGE
 
-echo -e " Message: $message"
+echo -e "Message: $message"
 
