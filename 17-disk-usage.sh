0713679d7bc7d8936a155aee0edddce7ae12da41
mallikarjunramireddy <mallikarjunrao.ramireddy@gmail.com>
2025-04-21 14:35:41 +0530

    malli-shell-practice

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-
-DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
-DISK_THRESHOLD=1
+# This script checks the disk space usage of the system and sends an alert if it exceeds a certain threshold.
+# It also provides a summary of the disk space usage.
+DISK_USAGE=$(df -hT|grep -vE 'temp|File')
+DISK_TRESHHOLD=1
 message=""
 
 while IFS= read line
-do
+do 
     usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+
     partition=$(echo $line | awk '{print $1F}')
-    if [ $usage -ge $DISK_THRESHOLD ]
+
+    if [ $usage -ge $DISK_TRESHHOLD ]
     then
-        message+="High Disk Usage on $partition: $usage <br>"
+        message+="High Disk Usage on $partition: $usage"
     fi
+
 done <<< $DISK_USAGE
 
 echo -e "Message: $message"
-
