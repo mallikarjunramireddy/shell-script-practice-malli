6bd6f47cd329019ad3baa97635c46ce8c28cbe15
mallikarjunramireddy <mallikarjunrao.ramireddy@gmail.com>
2025-04-21 13:06:11 +0530

    malli-shell-practice

---
+++ 15-delete-old-log-files.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+SOURCE_DIR="/tmp/logs"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+if [ ! d $SOURCE_DIR ]
+then
+    echo -e "$R ERROR:: Directory $SOURCE_DIR does not exist $N"
+    exit 1
+fi
+
+FILEs_TO_DELTE=$(find $SOURCE_DIR -type f -name "*.log" -mtime +14)
+
+while IFS= read -r line
+do 
+    echo " Deleting file is $line"
+    rm -rf $line
+
+done <<< $FILEs_TO_DELTE
+
