f5c21a25de38f52f90541e7e0397e6e4a168e5ab
mallikarjunramireddy <mallikarjunrao.ramireddy@gmail.com>
2025-04-25 11:45:52 +0530

    malli-shell-practice

--- roboshop.sh
+++ roboshop.sh
@@ -16,7 +16,7 @@ do
     # fi
     INSTANCE_TYPE="t2.micro"
 
-    aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]")
+    aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
 
     # IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     # echo "$i: $IP_ADDRESS"
