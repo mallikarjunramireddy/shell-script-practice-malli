d771254bec01487a81874da37a7a1a332b526412
mallikarjunramireddy <mallikarjunrao.ramireddy@gmail.com>
2025-04-25 12:04:26 +0530

    malli-shell-practice

--- roboshop.sh
+++ roboshop.sh
@@ -16,11 +16,12 @@ do
     # fi
     INSTANCE_TYPE="t2.micro"
 
+    echo "This is $i instance"
+    
     aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
 
     # IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
      #echo "$i: $IP_ADDRESS"
-    echo "This is $i instance"
     # #create R53 record, make sure you delete existing record
     # aws route53 change-resource-record-sets \
     # --hosted-zone-id $ZONE_ID \
